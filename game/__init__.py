0fa4fcf3af2356fa9ff78493ba1036abd339a4cc
Leandro Cardoso <leandrocardosodev@gmail.com>
2023-10-09 01:09:24 -0300

    World function realocate and function draw_window

--- game/__init__.py
+++ game/__init__.py
@@ -11,16 +11,11 @@ from game.models import *
 world = World()
 player = Player()
 
-# IMPORTS GAME CONTROLLERS:
-from game.controllers import *
-
 # TESTS:
 if __name__ == '__main__':
     d20 = Dice(20)
 
     player.set_name('Leandro')
 
-    history = find_history(1000)
-
     print(player.name, d20.roll())
-    print(history['title'])
+    world.draw_window()
--- game/models/world.py
+++ game/models/world.py
@@ -1,24 +1,47 @@
+import json, textwrap
+
+from config import GAME_HISTORY_PATH, GAME_HISTORY_NAMES
+
 class World():
     '''Generate game world.'''
     def __init__(self, name:str = None, location:str = None) -> None:
         self.name = name
         self.location = location
-        self.choice_ids = []
+        self.choice_ids = [1000]
+        self.history = None
+        self.find_history()
+
     def set_name(self, name:str) -> None:
         '''Set world name.'''
         self.name = name
+
     def set_location(self, location:str) -> None:
         '''Set world location.'''
         self.location = location
+
     def add_choice_id(self, choice_id:int) -> None:
         '''Add choice id in choice id list.'''
         self.choice_ids.append(choice_id)
 
-if __name__ == '__main__':
-    world = World(location = 'Green Valley')
-    print('Location:', world.location)
+    def find_history(self) -> None:
+        '''Find actual history point, based in choice id.'''
+        for history_name in GAME_HISTORY_NAMES:
+            file_path = GAME_HISTORY_PATH + '\\' + history_name
 
-    world.make_choice(2)
-    world.make_choice(4)
-    world.make_choice(1)
-    print('Choices:', world.choices)
+            with open(file_path, 'r', encoding = 'utf-8') as json_file:
+                history_to_check = json.load(json_file)
+                json_file.close()
+
+            if history_to_check['id'] == self.choice_ids[-1]:
+                self.history = history_to_check
+                break
+
+    def draw_window(self) -> None:
+        '''Draw game window in CMD.'''
+        chapter_number = self.history['chapter_number']
+        chapter_name = self.history['chapter_name']
+        chapter = f' CAPÍTULO {chapter_number}: {chapter_name} '
+        print(f'{chapter:=^100}\n')
+
+        text = self.history['description']
+        print(textwrap.fill(text, width = 100))
