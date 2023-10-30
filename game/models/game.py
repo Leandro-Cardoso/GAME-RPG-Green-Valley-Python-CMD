4ec6366a36caf730c9bef440ff6db4c63e236b86
Leandro Cardoso <leandrocardosodev@gmail.com>
2023-10-30 05:19:30 -0300

    Remake Game class

--- game/models/game.py
+++ game/models/game.py
@@ -1,30 +1,47 @@
 from screen import Screen
-from player import Player
 
 class Game():
     '''New game.'''
     def __init__(self) -> None:
         self.is_running = False
-
         self.screen = Screen()
-        self.player = Player()
+        self.error = ''
+        self.choiced_id = 0
         self.choiced_ids = [1000]
-
-    def open_menu(self) -> int:
-        '''Open game menu.'''
-        is_open = True
-        menu = Screen()
-        while is_open:
-            pass
-
+    
+    def dialogue(self) -> None:
+        '''Start dialogue with player.'''
+        try:
+            response = int(input('DIGITE UMA OPÇÃO: '))
+            if response in self.screen.choice_ids:
+                self.choiced_id = response
+            else:
+                self.error = f'A opção {response} não existe, tente outra.'
+        except:
+            self.error = 'Você precisa digitar um número inteiro.'
+    
+    def create_screen(self) -> None:
+        '''Create screen with player response.'''
+        self.screen = Screen(self.choiced_id)
+    
     def run(self) -> None:
         '''Run game.'''
         self.is_running = True
-        screen = Screen()
-        screen.draw()
-        #while self.is_running:
-            #response = self.open_menu()
+        while self.is_running:
+            # DRAW SCREEN:
+            self.screen.draw()
 
-    def add_choice_id(self, choice_id:int) -> None:
-        '''Add choice id in choice id list.'''
-        self.choiced_ids.append(choice_id)
+            # DRAW ERROR:
+            if self.error != '':
+                print(f'ERRO: {self.error}')
+
+            # START DIALOGUE:
+            self.dialogue()
+
+            # EXIT:
+            if self.choiced_id == 3:
+                self.is_running = False
+            
+            # CREATE SCREEN:
+            else:
+                self.create_screen()
